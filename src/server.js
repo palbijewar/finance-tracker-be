58a4229e07397710659bf854f25e922c2267ac35
Pal Bijewar <pal.bijewar@gmail.com>
2026-01-20 14:30:33 +0530

    fix: cors config

--- src/server.js
+++ src/server.js
@@ -7,7 +7,17 @@ require("dotenv").config();
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://finance-tracker-fe-blue.vercel.app",
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
+
 app.use(express.json());
 
 connectDB();
